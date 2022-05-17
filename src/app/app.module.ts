011884e9c1b56520a594a930f0bc668299f903f4
Corentin <corentincolnat@gmail.com>
2022-05-17 16:53:47 +0200

    Création page etudient intervenant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { PageEtudiantComponent } from './page-etudiant/page-etudiant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AccueilComponent
+    AccueilComponent,
+    PageEtudiantComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/page-etudiant/page-etudiant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-etudiant',
+  templateUrl: './page-etudiant.component.html',
+  styleUrls: ['./page-etudiant.component.scss']
+})
+export class PageEtudiantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

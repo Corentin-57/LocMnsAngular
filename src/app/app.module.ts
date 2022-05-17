a4dfd99247653214ffee83880bd565fa56658427
erdinc-agbulut <erdinc.agbulut@gmail.com>
2022-05-17 20:19:47 +0200

    pages connexion/footer/gestionnaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { PageGestionnaireComponent } from './page-gestionnaire/page-gestionnaire.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ContactComponent,
+    ConnexionComponent,
+    PageGestionnaireComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/page-gestionnaire/page-gestionnaire.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-gestionnaire',
+  templateUrl: './page-gestionnaire.component.html',
+  styleUrls: ['./page-gestionnaire.component.scss']
+})
+export class PageGestionnaireComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
